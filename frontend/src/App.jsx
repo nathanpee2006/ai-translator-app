48f527b7d5353462bb4c879379a7e2ec96b8ec7c
nathanpee2006 <nathanpee2006@gmail.com>
2025-08-12 21:43:38 +1200

    Created simple UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import axios from "axios";
+import { Textarea } from "./components/ui/textarea";
+import { Button } from "./components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [englishText, setEnglishText] = useState("");
+  const [language, setLanguage] = useState("");
 
   const fetchAPI = async () => {
     const response = await axios.get("http://localhost:3000/api");
@@ -16,29 +23,35 @@ function App() {
     fetchAPI();
   }, []);
 
+  function handleTranslate(e) {
+    e.preventDefault();
+    console.log(englishText);
+    console.log(language);
+  }
+
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <form onSubmit={handleTranslate}>
+      <h1>AI Translator App</h1>
+      <Textarea
+        placeholder="Enter text to translate"
+        value={englishText}
+        onChange={(e) => setEnglishText(e.target.value)}
+      ></Textarea>
+      <Select value={language} onValueChange={setLanguage}>
+        <SelectTrigger>
+          <SelectValue placeholder="Select a language" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="French">French</SelectItem>
+          <SelectItem value="Spanish">Spanish</SelectItem>
+          <SelectItem value="Japanese">Japanese</SelectItem>
+          <SelectItem value="German">German</SelectItem>
+          <SelectItem value="Italian">Italian</SelectItem>
+        </SelectContent>
+      </Select>
+      <Button onClick={handleTranslate}>Translate</Button>
+      <Textarea placeholder="Translated text"></Textarea>
+    </form>
   );
 }
 
